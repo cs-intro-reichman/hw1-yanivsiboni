86bfa62e2b369fcec72f1378effbc3b06837286d
יניב סיבוני <yanivsiboni@h-mhsb-nyyd-sl-ynyb.local>
2025-11-03 21:40:46 +0200

    Done

--- FVCalc.java
+++ FVCalc.java
@@ -3,9 +3,9 @@ public class FVCalc {
 	public static void main(String[] args){
 	int investedSum = Integer.parseInt(args [0]);
 	double annualRate = Double.parseDouble (args [1]);
-	double years = Double.parseDouble (args [2]);
+	int years = Integer.parseInt(args [2]);
 	int futureValue = (int) (investedSum * (Math.pow(1 + annualRate / 100.0, years)));
-	System.out.println("After " + years + ",years $" + investedSum + " saved at " + annualRate + "% will yield $" + futureValue);
+	System.out.println("After " + years + " years, $" + investedSum + " saved at " + annualRate + "% will yield $" + futureValue);
 
 
 	}
