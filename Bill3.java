c86f3cc20a0ecd19c800761001ea1f97b5994663
יניב סיבוני <yanivsiboni@h-mhsb-nyyd-sl-ynyb.local>
2025-11-03 22:02:00 +0200

    Donee

--- Bill3.java
+++ Bill3.java
@@ -6,8 +6,8 @@ public class Bill3 {
 	    String name2 = args[1];
 		String name3 = args[2];
 		int price = Integer.parseInt(args[3]);  
-		double split = price / 3;
-		Math.ceil(split);
+		double split = price / 3.0;
+		split = Math.ceil(split);
 		System.out.println("Dear " + name3 + ", " + name2 + ", " + "and " + name1 + ": pay " + split + " Shekels each ");
 	}
 }
